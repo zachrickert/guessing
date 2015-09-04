0abad9900a2f8d1bb80300219741e999b293c7ea
Zach Rickert <zachrickert@gmail.com>
2015-09-03 22:01:58 -0700

    add code to replay game.

--- guessing.py
+++ guessing.py
@@ -4,22 +4,29 @@
 import random
 
 def main():
-  target = random.randint(1, 10)
-  correct = False
+  replay = True
 
-  while  not correct:
-    guess = input("Please enter an number between 1 & 10: ")
+  while replay:
+    if replay:
+      target = random.randint(1, 10)
+      correct = False
 
-    if guess == target:
-      print ("Correct - Great Guess!!!")
-      correct = True
-    elif guess > target:
-      print("lower")
-    else:
-      print("higher")
+      while not correct:
+        if not correct:
+          guess = input("Please enter an number between 1 & 10: ")
 
-    if correct:
-      break
+          if guess == target:
+            print ("Correct - Great Guess!!!")
+            correct = True
+            replay_input = raw_input("Do you want to play again? (Y/N)")
+            if replay_input.lower() == "n":
+              replay = False
+
+          elif guess > target:
+            print("lower")
+          else:
+            print("higher")
+
 
   print ("End of Game")
 
